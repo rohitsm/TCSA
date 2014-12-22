f27d692a5d9dde054f585ca102f32e1a2ea0ffb1
Rohit Menon <rohitsm@gmail.com>
2014-12-23 01:52:19 +0800

    updated packages.sh

--- Code/packages.sh
+++ Code/packages.sh
@@ -1,4 +1,9 @@
+sudo apt-get install python-setuptools python-dev build-essential
+sudo apt-get install libmysqlclient-dev
+
+
 pip install Flask
 pip install flask-script
 pip install WTForms
 pip install flask-sqlalchemy
+sudo pip install MySQL-python
