813f2d2621330b782921fbae93a93079552fce66
Rohit Menon <rohitsm@gmail.com>
2014-12-22 19:39:04 +0800

    na

--- Code/Login/models.py
+++ Code/Login/models.py
@@ -1,18 +1,9 @@
 from app import db
 
+# class User(db.model):
 
+# 	# Setting the table name
+# 	__tablename__ = 'login1'
 
-
-class User(db.model):
-
-	# Setting the table name
-	__tablename__ = 'login1'
-
-	email = db.Column('email', db.String(120), primary_key=True)
-	password = db.Column('password', db.String())
-
-
-
-class Login1(db.Model):
-	email = db.Column(db.String(120),required=True, unique=True, primary_key=True)
-	password = db.Column(db.String(64), required=True))
+# 	email = db.Column('email', db.String(120), primary_key=True)
+# 	password = db.Column('password', db.String())
