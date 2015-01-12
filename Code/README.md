02046eefab09505cc4286711a9424dc267c346e6
Rohit Menon <rohitsm@gmail.com>
2015-01-12 15:57:49 +0800

    updated SQL schema. updated readme

--- Code/README.md
+++ Code/README.md
@@ -2,13 +2,12 @@
 
 **Note:** This is an initial commit and contains preliminary raw code. For updated version check `ross_v2`
 
-<<<<<<< HEAD
+
 *	_login/_ - Contains the related HTML and CSS code for the login page
-  *	Ross' Development Branch: `ross_v1`
-=======
-**STATUS:** Implemented and connected Dropbox API. 
+
+**STATUS:** Implemented and connected Dropbox API. Cleaning up and code restructuring in progress
+
 
 *	**login** - Contains the related HTML and CSS code for the login page
   *	Ross' Development Branch: `ross_v2`
->>>>>>> ross_v2
 
--- Code/login/schema.sql
+++ Code/login/schema.sql
@@ -2,6 +2,14 @@
 
 USE users;
 
+CREATE TABLE Login(
+	email VARCHAR(120) NOT NULL UNIQUE PRIMARY KEY,
+	-- Hashed password
+	password VARCHAR(100) NOT NULL
+	-- Hashed passphrase
+	passphrase VARCHAR(100) NOT NULL,
+) ENGINE=InnoDB ;
+
 -- CREATE TABLE Login_1(
 -- 	email VARCHAR(120) NOT NULL UNIQUE PRIMARY KEY,
 -- 	-- Hashed password
@@ -17,14 +25,14 @@ USE users;
 -- 	ON DELETE CASCADE
 -- )ENGINE=InnoDB;
 
-CREATE TABLE Profile(
-	email VARCHAR(120) NOT NULL UNIQUE PRIMARY KEY,
+-- CREATE TABLE Profile(
+-- 	email VARCHAR(120) NOT NULL UNIQUE PRIMARY KEY,
 	
-	-- Columns for access_tokens
-	dropbox BLOB,
-	gdrive BLOB,
+-- 	-- Columns for access_tokens
+-- 	dropbox BLOB,
+-- 	gdrive BLOB,
 	
-	FOREIGN KEY email(email) REFERENCES Login_1(email)
-	ON UPDATE CASCADE
-	ON DELETE CASCADE
-)ENGINE=InnoDB;
+-- 	FOREIGN KEY email(email) REFERENCES Login_1(email)
+-- 	ON UPDATE CASCADE
+-- 	ON DELETE CASCADE
+-- )ENGINE=InnoDB;
