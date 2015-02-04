9482c445876844dd974468e27fd7c553bdc291ca
Rohit Menon <rohitsm@gmail.com>
2015-02-04 23:10:18 +0800

    Separated user_info_service into separate function get_user_info() as per Google API documentation. Rarranged functions

--- Code/login/gdrive_conn.py
+++ Code/login/gdrive_conn.py
@@ -54,56 +54,6 @@ def get_gdrive_refresh_token():
 	print "Error no record of refresh_token found in DB. Not connected to Google Drive"
 	return None
 
-def gdrive_connect():
-	"""Interfaces with views.py
-
-	Returns:
-		User information as a JSON string if it exists otherwise None. 
-	"""
-	try:		
-		cred = get_gdrive_refresh_token()
-		print "gdrive_connect() - cred from db = ", cred
-		# Convert JSON represenation to an instance of 'OAuth2Credentials'
-		credentials = Credentials.new_from_json(cred)
-		
-		# No record found in DB
-		if credentials is None:
-			print "No records for credentials found in DB = none"
-			return None		
-
-		# Expired access_token
-		if credentials.access_token_expired:
-			# Get new access_token
-			print "credentials.access_token_expired"
-			credentials = refresh_access_token(credentials)
-
-		# Returns user information as a JSON object
-		user_info_service = build(
-			serviceName = 'drive', version = 'v2',
-			http = credentials.authorize(httplib2.Http()) )
-		
-		# user_info is of type dict 
-		user_info = user_info_service.files().list().execute()
-		# print "\n\nuser_info = ", json.dumps(user_info, indent=4, sort_keys=True)
-		return json.dumps(user_info)
-
-	except client.AccessTokenRefreshError as e:
-		flash('Drive access revoked!')
-		print "client.AccessTokenRefreshError", e
-		# User revoked access, so remove gdrive token from DB
-		set_gdrive_token(email, None)
-		return None
-
-	except AttributeError as e:
-		print "no record found in DB. \nArributeError", e
-		# No record exists in DB for GDrive
-		return None
-
-	except TypeError as e:
-		print "no record found in DB. \nTypeError", e
-		return None
-
-
 def refresh_access_token(cred):
 	""" Take credentials (with expired access token) as arg and return
 		credentials with renewed access token.
@@ -156,6 +106,79 @@ def refresh_access_token(cred):
 
 	except Exception, e:
 		print "Error: ", e
+		return None
+
+def get_user_info(credentials):
+	"""Send a request to the UserInfo API to retrieve the user's information.
+
+	Args:
+	credentials: oauth2client.client.OAuth2Credentials instance to authorize the
+	             request.
+	Returns:
+	User information as a dict.
+	"""
+	user_info_service = build(
+	  serviceName='oauth2', version='v2',
+	  http=credentials.authorize(httplib2.Http()))
+	user_info = None
+	try:
+		user_info = user_info_service.userinfo().get().execute()
+	except errors.HttpError, e:
+		logging.error('An error occurred: %s', e)
+	if user_info and user_info.get('id'):
+		return user_info
+	else:
+		raise NoUserIdException()
+
+def gdrive_connect():
+	"""Interfaces with views.py
+
+	Returns:
+		User information as a JSON string if it exists otherwise None. 
+	"""
+	try:		
+		cred = get_gdrive_refresh_token()
+		print "gdrive_connect() - cred from db = ", cred
+		# Convert JSON represenation to an instance of 'OAuth2Credentials'
+		credentials = Credentials.new_from_json(cred)
+		
+		# No record found in DB
+		if credentials is None:
+			print "No records for credentials found in DB = none"
+			return None		
+
+		# Expired access_token
+		if credentials.access_token_expired:
+			# Get new access_token
+			print "credentials.access_token_expired"
+			credentials = refresh_access_token(credentials)
+
+		# Returns user information as a JSON object
+		# user_info_service = build(
+		# 	serviceName = 'drive', version = 'v2',
+		# 	http = credentials.authorize(httplib2.Http()) )
+		
+		# # user_info is of type dict 
+		# user_info = user_info_service.files().list().execute()
+
+		user_info = get_user_info(credentials)
+		# print "\n\nuser_info = ", json.dumps(user_info, indent=4, sort_keys=True)
+		return json.dumps(user_info)
+
+	except client.AccessTokenRefreshError as e:
+		flash('Drive access revoked!')
+		print "client.AccessTokenRefreshError", e
+		# User revoked access, so remove gdrive token from DB
+		set_gdrive_token(email, None)
+		return None
+
+	except AttributeError as e:
+		print "no record found in DB. \nArributeError", e
+		# No record exists in DB for GDrive
+		return None
+
+	except TypeError as e:
+		print "no record found in DB. \nTypeError", e
 		return None
 
 @app.route('/gdrive-auth-finish')
