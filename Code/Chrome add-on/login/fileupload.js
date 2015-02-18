5776c1c75db0d465a70cfdbf3dd6455ec7183b2c
ayephyu14 <ayephyu@Ayes-MacBook-Pro.local>
2015-02-18 14:17:11 +0800

    feb 18

---
+++ Code/Chrome add-on/login/fileupload.js
@@ -0,0 +1,52 @@
+$(':file').change(function(){
+    var file = this.files[0];
+    name = file.name;
+    size = file.size;
+    type = file.type;
+
+    if(file.name.length < 1) {
+    }
+    /*else if(file.type != 'image/png' && file.type != 'image/jpg' && !file.type != 'image/gif' && file.type != 'image/jpeg' ) {
+        alert("File doesnt match png, jpg or gif");
+    }*/
+    else { 
+        $(':submit').click(function(){
+            var formData = new FormData($('*formId*')[0]);
+            $.ajax({
+                url: 'script',  //server script to process data
+                type: 'POST',
+                xhr: function() {  // custom xhr
+                    myXhr = $.ajaxSettings.xhr();
+                    if(myXhr.upload){ // if upload property exists
+                        myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // progressbar
+                    }
+                    return myXhr;
+                },
+                //Ajax events
+                success: completeHandler = function(data) {
+                    /*
+                    * workaround for crome browser // delete the fakepath
+                    */
+                    if(navigator.userAgent.indexOf('Chrome')) {
+                        var catchFile = $(":file").val().replace(/C:\\fakepath\\/i, '');
+                    }
+                    else {
+                        var catchFile = $(":file").val();
+                    }
+                    var writeFile = $(":file");
+                    writeFile.html(writer(catchFile));
+                    $("*setIdOfImageInHiddenInput*").val(data.logo_id);
+                },
+                error: errorHandler = function() {
+                    alert("Något gick fel");
+                },
+                // Form data
+                data: formData,
+                //Options to tell JQuery not to process data or worry about content-type
+                cache: false,
+                contentType: false,
+                processData: false
+            }, 'json');
+        });
+    }
+});
