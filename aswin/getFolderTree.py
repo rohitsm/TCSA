370cad2efb138f826b0aa670ae1dbcdb6aff218f
asetiadi001 <asetiadi001@e.ntu.edu.sg>
2015-03-05 02:02:39 +0800

    Rearrange some folder

--- aswin/getFolderTree.py
+++
@@ -1,8 +0,0 @@
-import cgi
-import cgitb; cgitb.enable()
-from lib.MongoDBWrapper import MongoDBWrapper
-
-if __name__== '__main__':
-    form        =cgi.FieldStorage()
-    email       =form['email']
-    print MongoDBWrapper().getFolderTree(email)
