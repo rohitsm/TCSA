d592bbe3a815dee6a3a36ab1fc05dbf70cfb0237
Rohit Menon <rohitsm@gmail.com>
2015-03-07 16:48:07 +0800

    Resolved merge conflicts'

--- Code/login/__init__.py
+++ Code/login/__init__.py
@@ -1,9 +1,9 @@
-
 # Flask
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # Load config file
 app.config.from_object('config')
@@ -11,21 +11,11 @@ app.config.from_object('config')
 # Connect to MySQL with the defaults
 db = SQLAlchemy(app)
 
+# Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 # Login App
 from login import views, models
 
-# app.run()
-# app.debug = True
-
-# if not app.debug:
-#     import logging
-#     from logging.handlers import RotatingFileHandler
-
-#     file_handler = RotatingFileHandler('errorlog.log', 'a', 1 * 1024 * 1024, 10)
-#     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    
-#     app.logger.setLevel(logging.INFO)
-#     file_handler.setLevel(logging.INFO)
-    
-#     app.logger.addHandler(file_handler)
-#     app.logger.info('Error Log')
+views.routes(app, login_manager)
