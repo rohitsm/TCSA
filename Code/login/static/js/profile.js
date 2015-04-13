69d873a8b9b166188054bc6098a28562451bf563
Rohit Menon <rohitsm@gmail.com>
2015-04-13 21:23:56 +0800

    Plugin: Syntax error and HTML fix

--- Code/login/static/js/profile.js
+++ Code/login/static/js/profile.js
@@ -1,6 +1,6 @@
 
 // Save email to browser's session storage on button click
-function (){
+function install(){
 	$('#myModal').modal('show');
 	// alert("The TCSA Chrome plugin is currently being reviewed by the Chrome Web Store. Please try again in a few days.");
 };
