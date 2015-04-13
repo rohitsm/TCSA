2e0abbf63e769b7fe575224d24a8988743de3a16
Rohit Menon <rohitsm@gmail.com>
2015-04-13 21:15:47 +0800

    Plugin: Added plugin installation instructions

--- Code/login/static/js/profile.js
+++ Code/login/static/js/profile.js
@@ -1,6 +1,10 @@
 
 // Save email to browser's session storage on button click
-function setEmail_ls(email){
+function (){
+	$('#myModal').modal('show');
+	// alert("The TCSA Chrome plugin is currently being reviewed by the Chrome Web Store. Please try again in a few days.");
+};
+
+window.onload = function() {
 	sessionStorage.setItem("Email_ls", email);
-	alert("The TCSA Chrome plugin is currently being reviewed by the Chrome Web Store. Please try again in a few days.");
-}
+};
