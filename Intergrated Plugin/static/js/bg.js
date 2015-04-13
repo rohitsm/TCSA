6ec354a0875ff20c4b64266552ef3328d6a5d2f6
Rohit Menon <rohitsm@gmail.com>
2015-04-13 19:03:44 +0800

    Removed old version of Integrated Plugin

--- Intergrated Plugin/static/js/bg.js
+++
@@ -1,7 +0,0 @@
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  //chrome.tabs.create({'url': chrome.extension.getURL('keyGen.html')}, function(tab) {
-  	chrome.tabs.create({'url': '../templates/test.html'}, function(tab) {
-    // Tab opened.
-  });
-});
